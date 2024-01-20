98d205ddae6d9ecab09c87081c256e60d23288cc
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-01-20 17:28:16 -0300

    update togglemode

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toggleMode() {
   const html = document.documentElement;
+  const root = document.querySelector(":root");
 
   html.classList.toggle("light");
 
@@ -8,8 +9,10 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png");
     img.setAttribute("alt", "Light theme profile picture");
+    root.style.setProperty("--bg-color", "rgb(239, 235, 239)");
   } else {
     img.setAttribute("src", "./assets/avatar-dark.png");
     img.setAttribute("alt", "Dark theme profile picture");
+    root.style.setProperty("--bg-color", "rgb(22, 22, 24)");
   }
 }
