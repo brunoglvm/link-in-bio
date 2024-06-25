f6cfaf59be34ddf4f40d627177f2fd0c42c0fe40
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-06-25 14:52:32 -0300

    refactor: reorder .

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function toggleMode() {
   const html = document.documentElement;
   const root = document.querySelector(":root");
+  const img = document.querySelector("#user img");
 
   html.classList.toggle("light");
 
-  const img = document.querySelector("#profile img");
-
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png");
-    img.setAttribute("alt", "Light theme profile picture");
+    img.setAttribute("alt", "Light theme user image");
     root.style.setProperty("--bg-color", "rgb(239, 235, 239)");
   } else {
     img.setAttribute("src", "./assets/avatar-dark.png");
-    img.setAttribute("alt", "Dark theme profile picture");
+    img.setAttribute("alt", "Dark theme user image");
     root.style.setProperty("--bg-color", "rgb(22, 22, 24)");
   }
 }
